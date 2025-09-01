2c0742c68fb97fef17e63fa0a6428d04762c22f5
brilliahib <brilliahib21@gmail.com>
2025-09-02 00:46:54 +0700

    feat: voice assistant with integrate to gen ai and speech

---
+++ src/app/dashboard/voice/page.tsx
@@ -0,0 +1,9 @@
+import DashboardVoiceAssistantWrapper from "@/components/organisms/dashboard/voice/DashboardVoiceAssistantWrapper";
+
+export default function DashboardVoiceAssistantPage() {
+  return (
+    <section>
+      <DashboardVoiceAssistantWrapper />
+    </section>
+  );
+}
